55f682854cdb0a9d253490f838a000c2bdc75476
Caroline Giju <290408247+carolgiju308@users.noreply.github.com>
2026-07-27 20:41:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #Blood donation eligibility checker
 age=int(input('Enter your age:'))
+
 if age>=18:
     weight=float(input('Enter your weight:'))
-  if:
-    print('You are not old enough')
-elif weight>=50:
-    print('You are elligible!!')
+  
+    if weight>=50:
+       print('You are elligible!!')
+    else:
+       print('You do not meet the minimum weight requirement.')
 else:
-    print('You do not fit into the required criteria:')
+    print('You do not fit into the required age criteria.')
+    
     
 
