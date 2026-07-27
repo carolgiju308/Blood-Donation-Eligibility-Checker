0ed86c3abbc20b07ef1a21560cd366cbf02331d4
Caroline Giju <290408247+carolgiju308@users.noreply.github.com>
2026-07-27 20:37:21 +0530

    Implement blood donation eligibility checker
    Fix eligibility checker logic for age and weight.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+#Blood donation eligibility checker
+age=int(input('Enter your age:'))
+if age>=18:
+    weight=float(input('Enter your weight:'))
+  if:
+    print('You are not old enough')
+elif weight>=50:
+    print('You are elligible!!')
+else:
+    print('You do not fit into the required criteria:')
+    
 
